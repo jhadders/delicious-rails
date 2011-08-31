5dab3db627a970102e7e8b0ea03ce54705204041
Adam Bair <adambair@gmail.com>
2011-08-30 23:17:19 -0600

    Lab 2 complete

---
+++ labs/solutions/02-setup-rspec-capybara/linkr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkr::Application.config.secret_token = '66e1ad3b0c4896a86e4e2abca7aaad3db43054aa50115b9ce62775360a4f6b0a88ea1a9e0ea6cce73903957e171f39118690231fbec8293cda08107c4dd793d7'
